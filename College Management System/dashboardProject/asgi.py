2a2f140db489d1f8182936b91bc2b7b169b51f37
shawket <shawket.itworld@gmail.com>
2024-05-01 22:57:31 +0600

    Project Upload

---
+++ College Management System/dashboardProject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dashboardProject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboardProject.settings')
+
+application = get_asgi_application()
